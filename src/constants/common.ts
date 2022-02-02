91d683504f4d30faa3324db473b48a427b6318b1
infin1t3 <jovanovicv90@gmail.com>
2022-02-02 14:41:59 +0100

    Remove unused code

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,13 +1,3 @@
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-
-const DEVNET_URL = "";
-const MAINNET_URL = "";
-
-export const ACTIVE_NETWORK = {
-  NAME: WalletAdapterNetwork.Mainnet,
-  URL: MAINNET_URL,
-};
-
 export const NOTIFICATION_TYPE = {
   SUCCESS: "success",
   INFO: "info",
